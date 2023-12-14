f96b2fd56e367d383decb10bb553ec5d7baf005c
Iago Ramos <111618125+IagoRamosH@users.noreply.github.com>
2023-12-14 15:20:37 -0300

    clinica
    clinica

---
+++ voll-mobile-app-TT/src/utils/mock.ts
@@ -0,0 +1,19 @@
+const depoimentos = [
+  {
+    id: 1,
+    titulo: 'Breno, 30 anos, Bahia/BH.',
+    text: 'Esse aplicativo simplificou minha vida! Marcar consultas nunca foi tão fácil. O Voll Agendamento de Consultas me ajuda a encontrar os melhores horários, me lembra das consultas pendentes e ainda fornece informações sobre os médicos e clínicas. Recomendo a todos que desejam uma experiência mais eficiência na gestão de suas consultas médicas.',
+  },
+  {
+    id: 2,
+    titulo: 'Júlio, 40 anos, São Paulo/SP.',
+    text: 'O Voll Agendamento de Consultas mudou de maneira como gerenciar minhas consultas médicas. Antes, eu tinha dificuldades para lembrar os horários e dados, mas agora, com o aplicativo, recebo lembretes automáticos e consigo reagendar facilmente quando necessário. Uma ferramenta indispensável para quem valoriza o próprio tempo e saúde!.',
+  },
+  {
+    id: 3,
+    titulo: 'Marcos, 50 anos, Joaõ Pessoa/PB.',
+    text: '"Estou muito satisfeito com o Aplicativo Voll Agendamento de Consultas! A praticidade de marcar minhas consultas pelo celular facilita muito a minha vida. Além disso, a interface é intuitiva e fácil de usar. Nunca mais me esqueci de uma consulta desde que comecei a usar o aplicativo. Recomendo a todos que buscam organização e comodidade na marcação de consultas médicas.',
+  }
+]
+
+export { depoimentos }
